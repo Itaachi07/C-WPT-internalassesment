9ccdf49e3a385e31bf20ebd7031f164340a5e8f6
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-06-03 18:04:52 +0530

    database complete

---
+++ database.sql
@@ -0,0 +1,14 @@
+--create Database--
+
+create databsse std;
+
+use std;
+
+create table student(
+    roll_no int primary key auto_increment,
+    name varchar(40) NOT NULL,
+    class varchar(40) NOT NULL,
+    division varchar(8) NOT NULL,
+    dateofbirth date NOT NULL,
+    parent_mobile_no bigint NOT NULL
+);
